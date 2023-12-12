50bc554d4fb17b6564ab4204ecfa1150f5a812e7
k0st0chkA-228 <128410969+k0st0chkA-228@users.noreply.github.com>
2023-12-12 17:25:29 +0400

    Update Laba_13.py

--- Laba_13.py
+++ Laba_13.py
@@ -4,15 +4,23 @@
 import csv
 
 
-m_and_w = 0
-survived = 0
+man_count = 0
+women_count = 0
+survived_man = 0
+survived_woman = 0
 with open('titanic.csv', newline='') as File:
     reader = csv.reader(File)
     for r in reader:
-        if (r[4] == 'female' or r[4] == 'male') and r[11] == 'Q':
-            m_and_w += 1
+        if r[4] == 'male' and r[11] == 'Q':
+            man_count += 1
             if r[1] == '1':
-                survived += 1
+                survived_man += 1
+        elif r[4] == 'female' and r[11] == 'Q':
+            women_count += 1
+            if r[1] == '1':
+                survived_woman += 1
 
-print('количество мужчин и женщин, севших в порту Квинстаун: ' + str(m_and_w))
-print('количество выживших мужчин и женщин, севших в порту Квинстаун: ' + str(survived))
+print('количество мужчин, севших в порту Квинстаун: ' + str(man_count),
+      '\nколичество женщин, севших в порту Квинстаун: ' + str(women_count))
+print('количество выживших мужчин, севших в порту Квинстаун: ' + str(survived_man),
+      '\nколичество выживших женщин, севших в порту Квинстаун: ' + str(survived_woman))
